f3696fc1654437a5d2d0d01f861a5f9d66bef6da
Luiz Fernando Oliveira Maciel <lfnando.nando@gmail.com>
2021-03-22 15:12:41 -0300

    puc AEDS

--- PUC AEDS I/lista2/ex4.c
+++ PUC AEDS I/lista2/ex4.c
@@ -13,6 +13,6 @@ int main() {
     } else if (num3 > num2 && num3 > num1) {
         printf("Entre %d, %d e %d, %d é o maior.", num1, num2, num3, num3);
     } else {
-        printf("Os três valores são iguais.");
+        printf("Dois ou mais valores são iguais.");
     }
 }
