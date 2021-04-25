95b49e84b34576ac248bfb0fa80ab921bada1da3
lfnand0 <lfnando.nando@gmail.com>
2021-04-24 22:19:55 -0300

    puc AEDS - 24/04/2021

---
+++ PUC-AEDS-I/lista6/Parte1/ex12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float power(float x, float z) {
+  float total = x;
+  if (z > 0) {
+    for (int i = 1; i < z; i++) {
+      total *= x;
+    }
+  } else if (z == 0) {
+    total = 1;
+  } else {
+    total = 1;
+    for (int i = 0; i > z; i--) {
+      total /= x;
+    }
+  }
+  return total;
+}
+
+int main() {
+  float x, z;
+  printf("Digite o valor de n e de z: ");
+  scanf("%f %f", &x, &z);
+  printf("%.1f elevado a %.1f é %.4f.\n", x, z, power(x,z));
+}
